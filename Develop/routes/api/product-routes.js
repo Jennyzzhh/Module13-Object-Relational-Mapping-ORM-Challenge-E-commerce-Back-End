9ed4debd7139e1fa090990a437eb6afbf4bc463f
Jennyzzhh <zzh19980929@gmail.com>
2023-03-15 12:53:46 -0400

    add product route

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,35 +4,36 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', async(req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
-      include: [{ model: Tag},{ model: Category }, {model: ProductTag}],
+      include: [{ model: Tag }, { model: Category }, { model: Tag, through: ProductTag }
+      ],
     });
-    res.status(200).json(categoryData);
+    res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 // get one product
-router.get('/:id', async(req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.update(req.body, {
-      where:{
-        id:req.params.id
+      where: {
+        id: req.params.id
       }
     })
-    if(!productData) {
-      res.status(404).json({message:'No product with this id'});
+    if (!productData) {
+      res.status(404).json({ message: 'No product with this id' });
       return;
     }
-    res.json({message:'This product has ids!'})
-  }catch (err) {
+    res.json({ message: 'This product has ids!' })
+  } catch (err) {
     res.status(400).json(err)
   }
 });
@@ -111,12 +112,12 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', async(req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
-  try{
-    const categoryData = await Category.destroy({
-      where:{
-        id:req.params.id
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
       }
     });
     res.status(200).json(productData)
